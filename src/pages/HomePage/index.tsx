cd10c87eee12ec8aa22d92e0a1e428c949f336d5
CODER <CODER@rivne.itstep.org>
2026-02-20 11:42:28 +0200

    Add CreateCarItem

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,6 +1,8 @@
-import type {ICarItem} from "../types/ICarItem.ts";
+import type {ICarItem} from "../../types/ICarItem.ts";
 import {useState} from "react";
 import {Select} from "antd";
+import ItemCar from "./ItemCar.tsx";
+import CreateCarItem from "./CreateCarItem.tsx";
 
 const HomePage = () =>
 {
@@ -55,6 +57,12 @@ const HomePage = () =>
         }
     }
 
+    const deleteCarHandler = (id: number) => {
+        // console.log("Delete item ", id);
+        const result = confirm("Ви дійсно бажаєте видалити")
+        if (result === true)
+        setCars(cars.filter(car => car.id !== id));
+    }
     return (
         <>
             <h1 className={"text-4xl font-bold text-center md-4"}>Головна сторінка</h1>
@@ -63,48 +71,25 @@ const HomePage = () =>
                 <Select defaultValue={"default"} style={{width: 100}}
                         onChange={(value:string)=>sortByPrice(value)}
                         options={[
-                    {
-                        value: "default",
-                        label: "За замовчуванням"
-                    },
+                            {
+                                value: "default",
+                                label: "За замовчуванням"
+                            },
                             {
                                 value: "asc",
                                 label: "Ціна: від меншої до більшої "
                             },
                             {
 
-                            value: "desc",
-                            label: "Ціна: від більшої до меншої "
+                                value: "desc",
+                                label: "Ціна: від більшої до меншої "
                             }
-                ]} />
+                        ]} />
             </div>
+            <CreateCarItem/>
             {cars.map(car =>
-                <div key={car.id} className="max-w-sm mb-4 w-full lg:max-w-full lg:flex">
-                    <div
-                        className="h-48 lg:h-auto lg:w-48 flex-none  bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden "
-                        style={{backgroundImage: `url('${car.image}')`}} title="Woman holding a mug">
-                    </div>
-                    <div
-                        className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-                        <div className="mb-8">
-                            <p className="text-sm text-gray-600 flex items-center">
-                                <svg className="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg"
-                                     viewBox="0 0 20 20">
-                                    <path
-                                        d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z"/>
-                                </svg>
-                                Members only
-                            </p>
-                            <div className="text-gray-900 font-bold text-xl mb-2">{car.mark} {car.model} ({car.year})
-                            </div>
-                            <p className="text-gray-700 text-base">{car.description}</p>
-
-                        </div>
-                        <div className="flex items-center">
-                            <p className={'font-bold text-lg text-gray-900'}>{car.price}$</p>
-                        </div>
-                    </div>
-                </div>
+                  <ItemCar key ={car.id} car = {car}
+                  deleteCar={deleteCarHandler}/>
             )}
 
         </>
